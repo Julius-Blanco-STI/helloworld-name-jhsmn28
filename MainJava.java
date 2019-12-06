078b3be6d2c2d676bc958a05b2f79635af0ed82e
jhsmn28 <58575760+jhsmn28@users.noreply.github.com>
2019-12-06 08:36:21 +0800

    Add files via upload

---
+++ MainJava.java
@@ -0,0 +1,12 @@
+package mainjava;
+
+
+public class MainJava {
+
+    public static void main(String[] args) {
+        String name= "Jhasmin";
+        
+            System.out.println("Hello World " + name);
+    }
+    
+}
